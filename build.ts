d111231e139c80664cce7f45bc7ac6cde6be461b
Lixquid <lixquid@lixquid.com>
2024-05-09 00:35:35 +0100

    Adjust formatting

--- build.ts
+++ build.ts
@@ -4,9 +4,9 @@ import { bundle } from "https://deno.land/x/emit/mod.ts";
 // Create the dist folder if it doesn't exist, or clean it if it does
 
 try {
-    await Deno.remove("dist", { recursive: true });
+	await Deno.remove("dist", { recursive: true });
 } catch {
-    /* ignore failure */
+	/* ignore failure */
 }
 await Deno.mkdir("dist");
 
@@ -14,41 +14,41 @@ await Deno.mkdir("dist");
 // and store a record of its filename, name and description.
 
 const scripts: {
-    filename: string;
-    name: string;
-    description: string;
+	filename: string;
+	name: string;
+	description: string;
 }[] = [];
 
 for await (const entry of Deno.readDir("src")) {
-    if (entry.isFile && entry.name.endsWith(".ts")) {
-        console.log("Transpiling", entry.name);
-        const { code } = await bundle(join("src", entry.name));
-        const filename = entry.name.replace(".ts", "");
-        await Deno.writeTextFile(join("dist", filename), code);
+	if (entry.isFile && entry.name.endsWith(".ts")) {
+		console.log("Transpiling", entry.name);
+		const { code } = await bundle(join("src", entry.name));
+		const filename = entry.name.replace(".ts", "");
+		await Deno.writeTextFile(join("dist", filename), code);
 
-        const srcCodeLines = (
-            await Deno.readTextFile(join("src", entry.name))
-        ).split("\n");
-        // Name is the first line of the file, minus the "// " prefix
-        const name = srcCodeLines[0].slice(3).trim();
-        // Description is the third line of the file onwards until the first
-        // non-comment line, with the "// " prefix removed from each line.
-        const descriptionLines = [];
-        for (let i = 2; i < srcCodeLines.length; i++) {
-            const line = srcCodeLines[i];
-            if (line.startsWith("// ")) {
-                descriptionLines.push(line.slice(3));
-            } else {
-                break;
-            }
-        }
+		const srcCodeLines = (
+			await Deno.readTextFile(join("src", entry.name))
+		).split("\n");
+		// Name is the first line of the file, minus the "// " prefix
+		const name = srcCodeLines[0].slice(3).trim();
+		// Description is the third line of the file onwards until the first
+		// non-comment line, with the "// " prefix removed from each line.
+		const descriptionLines = [];
+		for (let i = 2; i < srcCodeLines.length; i++) {
+			const line = srcCodeLines[i];
+			if (line.startsWith("// ")) {
+				descriptionLines.push(line.slice(3));
+			} else {
+				break;
+			}
+		}
 
-        scripts.push({
-            filename,
-            name,
-            description: descriptionLines.join("\n").trim(),
-        });
-    }
+		scripts.push({
+			filename,
+			name,
+			description: descriptionLines.join("\n").trim(),
+		});
+	}
 }
 
 // Create an index.html file in dist that lists all the scripts.
@@ -56,53 +56,53 @@ for await (const entry of Deno.readDir("src")) {
 // language=HTML
 const html = `<!DOCTYPE html>
 <html>
-    <head>
-        <title>Deno CLI Scripts</title>
-        <style>
-            body {
-                font-family: sans-serif;
-                background: #333;
-                color: #eee;
-            }
-            a {
-                color: #fff;
-            }
-            body > ul {
-                margin-top: 3em;
-                margin-bottom: 3em;
-            }
-            body > ul > li {
-                margin-bottom: 1em;
-            }
-            code {
-                background: #111;
-                padding: .1em .3em;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Deno CLI Scripts</h1>
-        <p>A collection of scripts for the CLI, using Deno.</p>
-        <p>
-            Run these scripts using <code>deno run
-            https://dcli.lix.cx/&lt;script-name&gt;</code>. All scripts accept
-            the <code>--help</code> flag.
-        </p>
-        <ul>
-            ${scripts
-                .map(
-                    ({ filename, name, description }) =>
-                        `<li>
-                            <a href="${filename}"><code>${filename}</code> - ${name}</a>
-                            <ul>
-                                <li>${description}</li>
-                            </ul>
-                        </li>`
-                )
-                .join("\n")}
-        </ul>
-        <p>Source code available <a href="https://github.com/lixquid/deno-dcli">here</a> under the MIT license.</p>
-    </body>
+	<head>
+		<title>Deno CLI Scripts</title>
+		<style>
+			body {
+				font-family: sans-serif;
+				background: #333;
+				color: #eee;
+			}
+			a {
+				color: #fff;
+			}
+			body > ul {
+				margin-top: 3em;
+				margin-bottom: 3em;
+			}
+			body > ul > li {
+				margin-bottom: 1em;
+			}
+			code {
+				background: #111;
+				padding: .1em .3em;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Deno CLI Scripts</h1>
+		<p>A collection of scripts for the CLI, using Deno.</p>
+		<p>
+			Run these scripts using <code>deno run
+			https://dcli.lix.cx/&lt;script-name&gt;</code>. All scripts accept
+			the <code>--help</code> flag.
+		</p>
+		<ul>
+			${scripts
+				.map(
+					({ filename, name, description }) =>
+						`<li>
+							<a href="${filename}"><code>${filename}</code> - ${name}</a>
+							<ul>
+								<li>${description}</li>
+							</ul>
+						</li>`,
+				)
+				.join("\n")}
+		</ul>
+		<p>Source code available <a href="https://github.com/lixquid/deno-dcli">here</a> under the MIT license.</p>
+	</body>
 </html>
 `;
 
--- src/t3.ts
+++ src/t3.ts
@@ -11,15 +11,15 @@
 import { parse } from "https://deno.land/std@0.204.0/flags/mod.ts";
 
 const args = parse(Deno.args, {
-    collect: ["ignore"],
-    boolean: ["help", "verbose", "dry-run"],
-    default: {
-        ignore: [".git", "node_modules"],
-    },
+	collect: ["ignore"],
+	boolean: ["help", "verbose", "dry-run"],
+	default: {
+		ignore: [".git", "node_modules"],
+	},
 });
 
 if (args["help"]) {
-    console.log(`t3: Text Template Transformer
+	console.log(`t3: Text Template Transformer
 
 Performs transformations on text files that contain template variables.
 
@@ -48,15 +48,15 @@ Options:
     --dry-run           Prints the files that would be modified, but does not
                         modify them.
 `);
-    Deno.exit(0);
+	Deno.exit(0);
 }
 
 const verbose = args["verbose"];
 const ignoredDirs = new Set(args["ignore"]);
 
 if (verbose) {
-    console.log("Ignored directories:");
-    console.log(ignoredDirs);
+	console.log("Ignored directories:");
+	console.log(ignoredDirs);
 }
 
 // Get a list of all files in the current directory and all subdirectories that
@@ -64,21 +64,21 @@ if (verbose) {
 
 const files = new Set<string>();
 async function scanDir(dir: string) {
-    for await (const entry of Deno.readDir(dir)) {
-        if (entry.isDirectory) {
-            if (!ignoredDirs.has(entry.name)) {
-                await scanDir(`${dir}/${entry.name}`);
-            }
-        } else {
-            files.add(`${dir}/${entry.name}`);
-        }
-    }
+	for await (const entry of Deno.readDir(dir)) {
+		if (entry.isDirectory) {
+			if (!ignoredDirs.has(entry.name)) {
+				await scanDir(`${dir}/${entry.name}`);
+			}
+		} else {
+			files.add(`${dir}/${entry.name}`);
+		}
+	}
 }
 await scanDir(".");
 
 if (args["verbose"]) {
-    console.log("Files to process:");
-    console.log(files);
+	console.log("Files to process:");
+	console.log(files);
 }
 
 // Get a list of all variables and placeholders in the files.
@@ -88,88 +88,88 @@ const placeholders = new Map<string, string[]>();
 const todoRegex = /TODO(_+)(.*?)\1/g;
 
 for await (const filename of files) {
-    const fileText = await Deno.readTextFile(filename);
-    const matches = Array.from(fileText.matchAll(todoRegex));
-    if (matches.length === 0) {
-        files.delete(filename);
-    } else {
-        for (const match of matches) {
-            const t = match[2];
-            if (t.startsWith("!")) {
-                const n = t.slice(1);
-                if (!placeholders.has(n)) {
-                    placeholders.set(n, []);
-                }
-                placeholders.get(n)!.push(filename);
-            } else {
-                variables.add(t);
-            }
-        }
-    }
+	const fileText = await Deno.readTextFile(filename);
+	const matches = Array.from(fileText.matchAll(todoRegex));
+	if (matches.length === 0) {
+		files.delete(filename);
+	} else {
+		for (const match of matches) {
+			const t = match[2];
+			if (t.startsWith("!")) {
+				const n = t.slice(1);
+				if (!placeholders.has(n)) {
+					placeholders.set(n, []);
+				}
+				placeholders.get(n)!.push(filename);
+			} else {
+				variables.add(t);
+			}
+		}
+	}
 }
 
 if (args["verbose"]) {
-    console.log("Variables:");
-    console.log(variables);
-    console.log("Placeholders:");
-    console.log(placeholders);
-    console.log("Files with variables:");
-    console.log(files);
+	console.log("Variables:");
+	console.log(variables);
+	console.log("Placeholders:");
+	console.log(placeholders);
+	console.log("Files with variables:");
+	console.log(files);
 }
 
 // Query the user for values for all variables.
 
 console.log(`Found ${variables.size} variable(s):`);
 for (const v of variables) {
-    console.log(`    ${v}`);
+	console.log(`    ${v}`);
 }
 console.log("Enter values for each variable. Leave blank to skip.");
 const values = new Map<string, string>();
 for (const v of variables) {
-    const value = prompt(`${v}:`);
-    if (value) {
-        values.set(v, value);
-    }
+	const value = prompt(`${v}:`);
+	if (value) {
+		values.set(v, value);
+	}
 }
 
 // Perform the substitutions.
 
 if (args["dry-run"]) {
-    console.log("Replacing variables in the following files:");
+	console.log("Replacing variables in the following files:");
 }
 for (const filename of files) {
-    const fileText = await Deno.readTextFile(filename);
-    const newText = fileText.replace(todoRegex, (original, __, t) => {
-        if (t.startsWith("!")) {
-            return original;
-        }
-        const value = values.get(t);
-        if (value) {
-            return value;
-        }
-        return original;
-    });
-    if (args["dry-run"]) {
-        console.log(`    ${filename}`);
-    } else {
-        await Deno.writeTextFile(filename, newText);
-    }
+	const fileText = await Deno.readTextFile(filename);
+	const newText = fileText.replace(todoRegex, (original, __, t) => {
+		if (t.startsWith("!")) {
+			return original;
+		}
+		const value = values.get(t);
+		if (value) {
+			return value;
+		}
+		return original;
+	});
+	if (args["dry-run"]) {
+		console.log(`    ${filename}`);
+	} else {
+		await Deno.writeTextFile(filename, newText);
+	}
 }
 
 // Alert the user about placeholders if any were found.
 
 if (placeholders.size > 0) {
-    console.log("┏━━━━━━━━━━━━━━━━━━━━━━━━━┓");
-    console.log("┃                         ┃");
-    console.log("┃  PLACEHOLDERS DETECTED  ┃");
-    console.log("┃                         ┃");
-    console.log("┗━━━━━━━━━━━━━━━━━━━━━━━━━┛");
-    console.log("");
-    console.log("Please replace the following placeholders with real values:");
-    for (const [placeholder, filenames] of placeholders) {
-        console.log(`    ${placeholder}:`);
-        for (const filename of filenames) {
-            console.log(`        ${filename}`);
-        }
-    }
+	console.log("┏━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+	console.log("┃                         ┃");
+	console.log("┃  PLACEHOLDERS DETECTED  ┃");
+	console.log("┃                         ┃");
+	console.log("┗━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+	console.log("");
+	console.log("Please replace the following placeholders with real values:");
+	for (const [placeholder, filenames] of placeholders) {
+		console.log(`    ${placeholder}:`);
+		for (const filename of filenames) {
+			console.log(`        ${filename}`);
+		}
+	}
 }
